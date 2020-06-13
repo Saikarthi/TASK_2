a46de7788eebb70d3f0556fb74ef6a0a02feb977
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-06-14 02:16:19 +0530

    mask

---
+++ TASK/Assets/script/collide_check.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collide_check : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D co)
+    {
+        if(co.gameObject.tag =="Player")
+        {
+            Debug.Log("1");
+            Destroy(this.gameObject);
+        }
+    }
+}
